4be0e3ff91e7557b9644491fe88f43f4897561a5
Junrong Fu <junrongfu6@gmail.com>
2023-01-16 21:03:12 +0800

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>HomePage</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
+        dicta iusto explicabo quibusdam neque? Adipisci doloribus dignissimos
+        temporibus accusantium inventore fuga, necessitatibus, perspiciatis id
+        ratione itaque eos natus atque repellendus.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
