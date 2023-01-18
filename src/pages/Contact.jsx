f8eb4ecd8c4781f1b507f098cab095b581a3a1a2
Junrong Fu <junrongfu6@gmail.com>
2023-01-18 13:06:10 +0800

    commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Contact = () => {
+  const queryString = useLocation().search;
+
+  const queryParams = new URLSearchParams(queryString);
+
+  const name = queryParams.get("name");
+
   return (
     <div>
-      <h2>Contact</h2>
+      <h2>Hey {name}, Contact us here...</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
         dicta iusto explicabo quibusdam neque? Adipisci doloribus dignissimos
