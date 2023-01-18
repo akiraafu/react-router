1cb54199b57f250f40c665294c316b96de4155ab
Junrong Fu <junrongfu6@gmail.com>
2023-01-18 10:10:12 +0800

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+//styles
+import "./home.css";
 
 const Home = () => {
+  const {
+    data: articles,
+    isPending,
+    error,
+  } = useFetch("http://localhost:3000/articles");
   return (
-    <div>
-      <h2>HomePage</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-        dicta iusto explicabo quibusdam neque? Adipisci doloribus dignissimos
-        temporibus accusantium inventore fuga, necessitatibus, perspiciatis id
-        ratione itaque eos natus atque repellendus.
-      </p>
+    <div className="home">
+      <h2>Articles</h2>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {articles &&
+        articles.map((article) => (
+          <div key={article.id} className="card">
+            <h3>{article.title}</h3>
+            <p>{article.author}</p>
+            <Link to={`/articles/${article.id}`}>Read more...</Link>
+          </div>
+        ))}
     </div>
   );
 };
